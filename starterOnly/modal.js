5179e197f65b441bc5b6ebfd847ebd7d9f5414c8
gohnzy <gregory.nuzzo@gmail.com>
2023-10-09 12:31:26 +0200

    responsive finished

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,9 +1,18 @@
+
+
+const x = document.getElementById("myTopnav");
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  var xx = document.getElementById("icon")
+  var xxx = document.getElementById("icon-link")
+  if (x.className === "main-navbar") {
     x.className += " responsive";
+    xx.style.color="white"
+    xxx.style.background="rgb(255, 0, 27)"
+
   } else {
-    x.className = "topnav";
+    x.className = "main-navbar";
+    xx.style.color="rgb(255, 0, 27)"
+    xxx.style.background="white"
   }
 }
 
